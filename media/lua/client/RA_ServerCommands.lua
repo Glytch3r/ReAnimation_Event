5aa61666a9b66ad631dc75d1aafa5a34cc047274
Pao <picchetti1998@gmail.com>
2023-04-08 18:32:25 +0200

    Reverted

--- media/lua/client/RA_ServerCommands.lua
+++ media/lua/client/RA_ServerCommands.lua
@@ -39,13 +39,44 @@ Events.OnPlayerUpdate.Add(function(player)
             end
         end
     end
+    
+    if player:getModData()['isScareCrow'] then
+        if player:getVariableBoolean('isScareCrow') == false then
+            player:setVariable('isScareCrow', 'true');
+            if isClient() then
+                sendClientCommand('Reanimation', 'isScareCrow', {isScareCrow = true})
+            end
+        end
+    else
+        if player:getVariableBoolean('isScareCrow') == true then
+            player:setVariable('isScareCrow', 'false');
+            if isClient() then
+                sendClientCommand('Reanimation', 'isScareCrow', {isScareCrow = false})
+            end
+        end
+    end
+    
+    if player:getModData()['isSpectre'] then
+        if player:getVariableBoolean('isSpectre') == false then
+            player:setVariable('isSpectre', 'true');
+            if isClient() then
+                sendClientCommand('Reanimation', 'isSpectre', {isSpectre = true})
+            end
+        end
+    else
+        if player:getVariableBoolean('isSpectre') == true then
+            player:setVariable('isSpectre', 'false');
+            if isClient() then
+                sendClientCommand('Reanimation', 'isSpectre', {isSpectre = false})
+            end
+        end
+    end
 end)
 
 
 Commands.Reanimation.isUndead = function(args)
     local source = getPlayer();
     local player = getPlayerByOnlineID(args.id)
-
     if source ~= player then
         if args.isUndead then
             player:setVariable('isUndead', 'true');
@@ -55,6 +86,28 @@ Commands.Reanimation.isUndead = function(args)
     end
 end
 
+Commands.Reanimation.isScareCrow = function(args)
+    local source = getPlayer();
+    local player = getPlayerByOnlineID(args.id)
+    if source ~= player then
+        if args.isScareCrow then
+            player:setVariable('isScareCrow', 'true');
+        else
+            player:setVariable('isScareCrow', 'false');
+        end
+    end
+end
+Commands.Reanimation.isSpectre = function(args)
+    local source = getPlayer();
+    local player = getPlayerByOnlineID(args.id)
+    if source ~= player then
+        if args.isSpectre then
+            player:setVariable('isSpectre', 'true');
+        else
+            player:setVariable('isSpectre', 'false');
+        end
+    end
+end
 Events.OnServerCommand.Add(function(module, command, args)
 	if Commands[module] and Commands[module][command] then
 		Commands[module][command](args)
